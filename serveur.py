250112b44fa92b146dcf0c19ba55070a22d68a9f
Jules Caesar <julescaesar42@gmail.com>
2020-05-19 11:29:29 +0200

    Nettoyage serveur.py

--- serveur.py
+++ serveur.py
@@ -122,17 +122,9 @@ def pokemons(nom_pokemon):
 
   return "".join(html)
 
-
 @app.route('/style.css')
 def css():
   return app.send_static_file("style.css")
-
-@app.route('/resultat', methods=['POST'])
-def resultat():
-  result = request.form
-  n = result['nom']
-  p = result['prenom']
-  return render_template("resultat.html", nom=n, prenom=p)
 
 app.run(debug=True)
 
